e33aa7c1395b6a7f4a46c605eeb6d95f689b1058
kordaro mitchell <kody_mitchell@ymail.com>
2019-08-13 14:27:48 -0400

    app model.py added

--- tunr_app/models.py
+++ tunr_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    photo_url = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255)
+    address = models.CharField(max_length=310)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='doctors')
+
+    def __str__(self):
+        return self.name
+
+class Pharmacy(models.Model):
+    name = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255)
+    address = models.CharField(max_length=310)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='pharmacies')
+
+    def __str__(self):
+        return self.name
