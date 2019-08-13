16bd9466408948e49c8d5217941909a5da8cfb77
kordaro mitchell <kody_mitchell@ymail.com>
2019-08-13 14:56:47 -0400

    tunr_app/views.py added

--- tunr_app/views.py
+++ tunr_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import LocationSerializer, DoctorSerializer, PharmacySerializer
+from .models import Location, Doctor, Pharmacy
 
-# Create your views here.
+
+class LocationView(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class DoctorView(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+class PharmacyView(viewsets.ModelViewSet):
+    queryset = Pharmacy.objects.all()
+    serializer_class = PharmacySerializer
