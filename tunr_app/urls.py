5b2bd7afe786d2546df75ea83a05cc25fb049e76
kordaro mitchell <kody_mitchell@ymail.com>
2019-08-13 15:11:21 -0400

    did something with app urls.py

--- tunr_app/urls.py
+++ tunr_app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('locations', views.LocationView)
+router.register('doctors', views.DoctorView)
+router.register('pharmacies', views.PharmacyView)
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
