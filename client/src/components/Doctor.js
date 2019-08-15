57b69e47241157bda6be361383cbc2db1af25d2b
kordaro mitchell <kody_mitchell@ymail.com>
2019-08-15 10:14:01 -0400

    doctor component created

--- client/src/components/Doctor.js
+++ client/src/components/Doctor.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Doctor extends Component {
+    state = {
+        doctor: {}
+
+    }
+
+    componentDidMount(){
+        this.getDoctor();
+    }
+
+    getDoctor = () => {
+        axios.get(`/api/v1/doctor/${this.props.match.params.id}/`)
+            .then((res) => {
+                this.setState({
+                    doctor: res.data,
+                });
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     render() {
         return (
             <div>
-                
+                <h1>{this.state.doctor.name}</h1>
             </div>
         )
     }
